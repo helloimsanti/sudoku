eb381fcc2602112cdd88331560aee40563c7c482
5anti <80968125+5anti@users.noreply.github.com>
2024-04-24 16:24:40 -0400

    Added the "select" method to board.py

--- board.py
+++ board.py
@@ -6,6 +6,7 @@ class Board:
 
     def __init__(self, difficulty='easy'):
         # Screen settings
+        self.COLOR = (255, 255, 255)
         self.WIDTH = 660
         self.HEIGHT = 780
         self.SCREEN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
@@ -20,6 +21,14 @@ class Board:
         self.SUBLINE_COLOR = (64, 64, 64)
         self.LINE_WIDTH = 6
         self.SUBlINE_WIDTH = 2
+
+        # Select box settings
+        self.SELECT_COLOR = (255, 0, 0)
+        self.SELECT_LEFT = 0
+        self.SELECT_TOP = 0
+        self.SELECT_WIDTH = 73
+        self.SELECT_HEIGHT = 73
+        self.SELECT_BORDER_WIDTH = 2
 
         # Game difficulty
         self.difficulty = difficulty
@@ -87,4 +96,30 @@ class Board:
             else:
                 self.SUBlINE_WIDTH = 2
 
-        pygame.display.update()
+    def select(self):
+        """Marks the cell at (row, col) in the board
+        as the current selected cell. Once a cell has
+        been selected, the user can edit its value or
+        sketched value."""
+        
+        # The select box
+        select_coords = (self.SELECT_LEFT, self.SELECT_TOP, self.SELECT_WIDTH, self.SELECT_HEIGHT)
+        pygame.draw.rect(self.SCREEN, self.SELECT_COLOR, select_coords, width=self.SELECT_BORDER_WIDTH)
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                # Moves select box up if the up arrow key or the "w" key is pressed.
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    self.SELECT_TOP -= 73
+                
+                # Moves the select box down if the down arrow key or the "s" key is pressed.
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    self.SELECT_TOP += 73
+                
+                # Moves the select box left if the left arrow key or the "a" key is pressed.
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    self.SELECT_LEFT -= 73
+                
+                # Moves the select box right if the right arrow key or the "s" key is pressed.
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    self.SELECT_LEFT += 73
